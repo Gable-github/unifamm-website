1f35f7cb306030b89306055d9ee3ea9fa90eb14e
Gabryel Soh <gabryelsoh@gmail.com>
2020-07-19 17:43:00 +0800

    Adapted to mobile draft 1

--- about.js
+++ about.js
@@ -62,4 +62,16 @@ $(document).ready(function(){
     };
   });
 
+  /// When you click everywhere in the document
+  $(document).click(function (event) {
+
+  /// If *navbar-collapse* is not among targets of event
+    if (!$(event.target).is('.navbar-collapse')) {
+
+      /// Collapse every *navbar-collapse*
+      $('.navbar-collapse').collapse('hide');
+
+    };
+  });
+
 });
--- ingredients.js
+++ ingredients.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
     opacity: 1
   }, 1000);
 
+  /// When you click everywhere in the document
+  $(document).click(function (event) {
+
+  /// If *navbar-collapse* is not among targets of event
+    if (!$(event.target).is('.navbar-collapse *')) {
+
+      /// Collapse every *navbar-collapse*
+      $('.navbar-collapse').collapse('hide');
+
+    }
+  });
+
 });
